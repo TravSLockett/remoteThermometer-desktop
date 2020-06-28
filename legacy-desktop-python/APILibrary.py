f5b3d50d676e9c5915d41ae3f38ed8ad808a5680
Mark Kornfeld <10789113+kornfeldm@users.noreply.github.com>
2020-06-28 01:48:11 -0400

    Legacy python code moved
    This branch will store the legacy python-based desktop app. It has been replaced and is here only for reference. Please note that we switched codebases during development so many features were not fully implemented for this version and others use obsolete API function and therefore no longer work.

---
+++ legacy-desktop-python/APILibrary.py
@@ -0,0 +1,22 @@
+import requests
+
+#This file contains additional functions used to communicate with the API
+
+def makeJSON(temperature_array):
+    JSONOutput = {'cpu':temperature_array[0], 'gpu':temperature_array[1]}
+    return JSONOutput
+  
+  
+#Source Referenced: https://www.w3schools.com/python/ref_requests_post.asp
+def sendDataToServer(JSONInput):
+    #Try to connect to the server, send the data and return a reply
+    try: 
+        url = 'http://[IP]:1205/user/push'
+        myobj = JSONInput
+        x = requests.post(url, data = JSONInput)
+        return x
+    #If this fails, it's likely a bad connection
+    except:
+        print("ERROR: SERVER CONNECTION IS BAD -> Did you replace the IP?")
+        return None
+        
---
+++ legacy-desktop-python/README.md
@@ -0,0 +1,12 @@
+# remoteThermometer-desktop
+
+### How to test (Only works on Windows):
+
+* Open PowerShell as Administrator
+* If this is your first time, do the following:
+  * Run "pip install wmi" in PowerShell (Used to read temperatures)
+  * Run "pip install pygubu" in PowerShell (Used for GUI - may cause compile errors without it)
+  * Run "pip install requests" in PowerShell (Used to communicate with the server)
+  * Optional: Set OpenHardwareMonitor to "launch minimized" (Hides OpenHardwareMonitor in the background during execution)
+* Navigate to the project folder and enter "python Main.py"
+* Remember to replace "[IP_ADDRESS]" (in APILibrary, line 14) with the IP of the server before running
